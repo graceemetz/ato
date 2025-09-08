e811d0bd7cc100af9af675eaa788ea34849259e7
graceemetz <graceemetz@gmail.com>
2025-09-07 20:48:02 -0400

    Server hosting added.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
 
 app.get('', (req, res) => {
     res.send('Placeholder');
